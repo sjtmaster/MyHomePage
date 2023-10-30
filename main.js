ee23004200f0b27cee18165348d00fff29fc1363
sjtmaster <sjtmaster@163.com>
2023-10-30 15:23:51 +0800

    json文件import

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert('Hi！')
+import myInfo from './Static/Json/MyInfo.json' assert { type:'json' };
+import myThchStack from './Static/Json/MyThchStack.json' assert { type:'json' };
+import myHobby from './Static/Json/MyHobby.json' assert {type:'json'};
+import about from './Static/Json/About.json' assert {type:'json'};
+
+console.log(myInfo);
+console.log(myThchStack);
+console.log(myHobby);
+console.log(about);
